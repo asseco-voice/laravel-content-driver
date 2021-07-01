78ec3639731ef2dae6a5ba65d2398df57cf28c64
Filip Visic <60132037+assefvisic@users.noreply.github.com>
2021-07-01 13:47:14 +0200

    zaboravio disk obrisati

--- config/filesystems.php
+++ config/filesystems.php
@@ -1,13 +1,11 @@
 <?php
 
 return [
-    'disks' => [
         'content' => [
             'driver' => 'content-file-storage',
             'token' => '',
             'baseURL' => env('FILESYSTEM_BASE_URL'),
             'baseRestAPIUrl' => env('FILESYSTEM_BASE_REST_API_URL'),
             'defaultRepository' => env('FILESYSTEM_DEFAULT_REPOSITORY'),
-        ],
-    ],
+        ]
 ];
