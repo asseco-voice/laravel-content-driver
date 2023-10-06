7478d6ffbef11838610fcab341c21034614ccf1c
Josip Milotić <josip.milotic@asseco-see.hr>
2023-10-06 10:12:41 +0200

    Added flag to toggle if driver should be added

--- config/filesystems.php
+++ config/filesystems.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Str;
 
 return [
     'content' => [
-        'driver'     => 'content-file-storage',
-        'api_url'    => env('CONTENT_API_URL'),
-        'repository' => env('CONTENT_REPOSITORY', 'dms'),
-        'root'       => env('CONTENT_ROOT', Str::snake(env('APP_NAME'))),
+        'driver'        => 'content-file-storage',
+        'api_url'       => env('CONTENT_API_URL'),
+        'repository'    => env('CONTENT_REPOSITORY', 'dms'),
+        'root'          => env('CONTENT_ROOT', Str::snake(env('APP_NAME'))),
+        'should_extend' => true
     ],
 ];
